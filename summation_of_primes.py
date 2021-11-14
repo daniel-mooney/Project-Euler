401d4309fab27acbd79a1fb73eb9c76ab154b9c0
daniel-mooney <daniel.mooney180@gmail.com>
2021-11-14 18:25:14 +1100

    completed summation_of_primes

--- summation_of_primes.py
+++ summation_of_primes.py
@@ -1,3 +1,26 @@
 def prime_sum(n: int):
-    # Finds the sum of all the primes less than of equal to n
-    pass
+    # Takes the sum of all primes up to n
+
+    # Sieve of Eratosthenes
+    prime_list = [True for i in range(n)]    
+    p = 2
+
+    while (p ** 2 <= n):
+        if prime_list[p] == True:
+            for i in range(p ** 2, n, p):
+                prime_list[i] = False
+        p += 1
+    
+    prime_list[0] = False
+    prime_list[1] = False
+
+    # Sum primes
+    prime_sum = 0
+    for i, prime in enumerate(prime_list):
+        if prime:
+            prime_sum += i
+    
+    return prime_sum
+
+
+print(prime_sum(2000000))
